a0f798247bf86d520bd3ea6ccc26657b2167bf58
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-03-04 13:37:15 +0700

    Update data.js

--- data.js
+++ data.js
@@ -309,7 +309,7 @@ async function fetchGoogleSearchData(url) {
       if (maxResults) googleSearchURL += `&maxResults=${maxResults}`;
     } else {
       const googleAPIKey = apikey[Math.floor(Math.random() * apikey.length)];
-      const googleCX = tbm === "nws" ? "f7113f6d71c8f48c8" : "435bdb05f0b5e47bb";
+      const googleCX = tbm === "nws" ? "f7113f6d71c8f48c8&dateRestrict=d7" : "435bdb05f0b5e47bb";
       googleSearchURL = `https://www.googleapis.com/customsearch/v1?key=${googleAPIKey}&cx=${googleCX}&q=${encodeURIComponent(query)}&start=${startIndex}`;
       
       if (gl) googleSearchURL += `&gl=${gl}`;
